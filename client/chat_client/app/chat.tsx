d8ac056ae4bbe5fc1026d596f7dfb5f809f08595
Błażej Kubica <b.j.kubica@student.tudelft.nl>
2026-02-05 15:53:15 +0100

    Added sending messages in the chat

--- client/chat_client/app/chat.tsx
+++ client/chat_client/app/chat.tsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from "react";
 import { getSocket } from "@/lib/socket";
 
 interface Message {
+    id: number;
     message: string;
     sender: string;
 }
 
 export default function Chat() {
-    const [messages, setMessages] = useState<Message[]>([])
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [input, setInput] = useState("");
 
     useEffect(() => {
         const socket = getSocket();
@@ -25,17 +27,36 @@ export default function Chat() {
             console.log("Message:", data);
         });
 
+        socket.on("all_messages", (data: Message[]) => {
+            setMessages(data);
+            console.log("Loaded all messages");
+        });
+
         return () => {
             socket.off("message");
             socket.disconnect();
         };
     }, []);
 
-    return <div className="flex flex-col">
-        {
-            messages.map(message => (
-                <div key={message.message}>Message: {message.message} From: {message.sender}</div>
-            ))
-        }
+    const sendMessage = () => {
+        console.log("I'am pressed!")
+        
+        if (!input.trim()) return;
+
+        const socket = getSocket();
+
+        socket.emit("message", input)
+    }
+
+    return <div className="flex flex-row h-screen">
+        <div className="flex flex-col w-1/2 p-5 gap-2">
+            {messages.map(message => (
+                <div key={message.id}>Message: {message.message} From: {message.sender}</div>
+            ))}
+        </div>
+        <div className="flex flex-col gap-3 items-center w-1/2 p-5 bg-amber-50">
+            <input value={input} className="w-full rounded-2xl bg-black text-white" onChange={e => setInput(e.target.value)}/>
+            <button onClick={sendMessage} className="bg-black rounded-2xl w-1/12">Send</button>
+        </div>
     </div>;
 }
--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 import { Server } from "socket.io";
 const messages = [];
+const map = new Map();
+var countUsers = 0;
 
 const io = new Server(8080, {
   // options
 });
 
 io.on("connection", (socket) => {
-    var count = 1;
-    const welcome_message = {message: "Welcome to the chat!", sender: "Server"};
-    messages.push(welcome_message);
-    io.emit("message", welcome_message);
-    setInterval(() => {
-        io.emit("message", {message: "Interval message: " + count, sender: "Server"});
-        count++;
-    }, 2000)
+    countUsers++;
+    console.log("User connected")
+    map.set(socket, countUsers);
+
+    socket.emit("all_messages", messages);
+
+    socket.on("message", (data) => {
+        console.log("recieved")
+        const message = {id: messages.length, message: data, sender: "User" + map.get(socket)};
+        messages.push(message)
+        io.emit("message", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected User" + map.get(socket))
+        map.delete(socket);
+    })
 });
